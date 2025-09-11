60129965b627d08a8569d0c3ef68715c64bbbe4b
naoto9852 <ldsan.m.52@gmail.com>
2025-09-11 21:43:56 +0900

    Self Introduction

---
+++ Hello World_NaotoMiura/src/sample/SelfIntroduction.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class SelfIntroduction {
+	
+	public static void main(String[] args) {
+		
+		String name = "三浦　直人";
+		int age = 27;
+		String hobby = "将棋";
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
+		//コメント内容
+		/*sys + command + spaceでSystem .out .println();を表示*/
+		
+	}
+
+}
