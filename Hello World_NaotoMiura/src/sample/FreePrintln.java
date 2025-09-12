e1f50472f94119b5b56a39364e402d31caceb1fa
naoto9852 <ldsan.m.52@gmail.com>
2025-09-12 10:44:49 +0900

    課題の進め方と調べ方

---
+++ Hello World_NaotoMiura/src/sample/FreePrintln.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class FreePrintln {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("water");
+		System.out.println("iphone");
+		System.out.println("light");
+		System.out.println("book");
+		
+	}
+
+}
