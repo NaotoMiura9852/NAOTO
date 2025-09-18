530a0a88b5b41cfe6814070255f1051f5a033328
naoto9852 <ldsan.m.52@gmail.com>
2025-09-18 22:33:54 +0900

    基本概念について

---
+++ Hello World_NaotoMiura/src/curriculum_B/Employee.java
@@ -0,0 +1,23 @@
+package curriculum_B;
+
+public class Employee {
+	
+	
+	//Q1 基本概念
+	private String name;
+	
+	public void Name(String name) {
+		this.name = name;
+	}
+	
+	public String Name() {
+		return this.name;
+	}
+	
+	public void work() {
+		System.out.println(name + "は働いています。");
+	}
+	
+	
+
+}
---
+++ Hello World_NaotoMiura/src/curriculum_B/Question6.java
@@ -0,0 +1,16 @@
+package curriculum_B;
+
+public class Question6 {
+	
+	public static void main(String[] args) {
+		
+		//Q1 基本概念
+		Employee emp = new Employee();
+		
+		emp.Name("山田太郎");
+		
+		emp.work();
+		
+	}
+
+}
