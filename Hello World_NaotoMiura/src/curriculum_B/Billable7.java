91629c9978d9c2173db3281586afc5a845a9e1ea
naoto9852 <ldsan.m.52@gmail.com>
2025-09-28 21:47:46 +0900

    SOLIDに関する課題

---
+++ Hello World_NaotoMiura/src/curriculum_B/Billable7.java
@@ -0,0 +1,8 @@
+package curriculum_B;
+
+public interface Billable7 {
+	
+	String getName();
+	int calculateSalary();
+
+}
---
+++ Hello World_NaotoMiura/src/curriculum_B/SalaryReporter.java
@@ -0,0 +1,13 @@
+package curriculum_B;
+
+import java.util.List;
+
+public class SalaryReporter {
+	
+	public void report(List<Billable7> employees) {
+		for (Billable7 e : employees) {
+			System.out.println(e.getName() + "の給料は" + e.calculateSalary() + "円");
+		}
+	}
+
+}
